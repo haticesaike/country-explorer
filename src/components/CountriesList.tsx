a110aee7176518ca93509e8e9387884a7b80e4e4
Hatice Saike <haticesaike374@gmail.com>
2024-02-18 23:32:56 +0300

    added randomColors

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -6,7 +6,7 @@ import { GET_COUNTRIES } from "../api/queries.ts";
 import "../css/countriesList.css";
 import { useAtom } from "jotai";
 import { selectedItemAtom } from "../jotai/atoms";
-import colors from "../constants/colors.tsx";
+import colors, { selectedColors } from "../constants/colors.tsx";
 
 function CountriesList() {
   const { loading, error, data } = useQuery(GET_COUNTRIES);
@@ -16,6 +16,18 @@ function CountriesList() {
   }>({});
   const [groupCriteria, setGroupCriteria] = useState<string>("all");
   const [selectedItem, setSelectedItem] = useAtom(selectedItemAtom);
+  const [currentSelectedColor, setCurrentSelectedColor] = useState<string>(
+    colors.selectColor1,
+  );
+  const randomColors = () => {
+    const len = selectedColors.length;
+    const index = Math.floor(Math.random() * len);
+    let color = selectedColors[index];
+    currentSelectedColor === color
+      ? randomColors()
+      : setCurrentSelectedColor(color);
+  };
+
   useEffect(() => {
     if (data) {
       setCountries(data.countries);
@@ -75,73 +87,92 @@ function CountriesList() {
   if (error) return <p>Error fetching countries: {error.message}</p>;
 
   return (
-    <div className="scroll-content">
-      <h3>List of Countries</h3>
-      <Search
-        onSearch={handleSearch}
-        onShowAll={() => {
-          setCountries(data.countries);
-          setGroupCriteria("all");
-          setSelectedItem(null);
-        }}
-      />
-      <select
-        onChange={(e) => setGroupCriteria(e.target.value)}
-        value={groupCriteria}
-        className="select-box"
-      >
-        <option value="all">All</option>
-        <option value="continent">Continent</option>
-        <option value="currency">Currency</option>
-      </select>
-      {groupCriteria !== "all"
-        ? Object.entries(groupedCountries).map(([key, countries]) => (
-            <div key={key} className="country">
-              <h4>{key}</h4>
-              <ul>
-                {countries.map((country) => (
-                  <li
-                    key={country.code}
-                    onClick={() => setSelectedItem(country)}
-                    style={
-                      selectedItem?.code === country.code
-                        ? { backgroundColor: colors.selectColor }
-                        : undefined
-                    }
-                  >
-                    <img
-                      width={20}
-                      height={20}
-                      src={`https://raw.githubusercontent.com/onramper/small-open-datasets/master/rendered-country-flags/flags/${country.code}.png`}
-                      alt={country.code}
-                    />
-                    {country.name} - Capital: {country.capital}, Currency:{" "}
-                    {country.currency}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))
-        : countries?.map((country) => (
-            <li
-              key={country.code}
-              onClick={() => setSelectedItem(country)}
-              style={
-                selectedItem?.code === country.code
-                  ? { backgroundColor: colors.selectColor }
-                  : undefined
-              }
-            >
-              <img
-                width={20}
-                height={20}
-                src={`https://raw.githubusercontent.com/onramper/small-open-datasets/master/rendered-country-flags/flags/${country.code}.png`}
-                alt={country.code}
-              />
-              {country.name} - Capital: {country.capital}, Currency:{" "}
-              {country.currency}
-            </li>
-          ))}
+    <div>
+      <div className="top-side">
+        <h3>List of Countries</h3>
+        <Search
+          onSearch={handleSearch}
+          onShowAll={() => {
+            setCountries(data.countries);
+            setGroupCriteria("all");
+            setSelectedItem(null);
+          }}
+        />
+        <select
+          onChange={(e) => setGroupCriteria(e.target.value)}
+          value={groupCriteria}
+          className="select-box"
+        >
+          <option value="all">All</option>
+          <option value="continent">Continent</option>
+          <option value="currency">Currency</option>
+        </select>
+      </div>
+      <div className="bottom-side scroll-content ">
+        {groupCriteria !== "all"
+          ? Object.entries(groupedCountries).map(([key, countries]) => (
+              <div key={key} className="country">
+                <h4>{key}</h4>
+                <ul>
+                  {countries.map((country) => (
+                    <li
+                      key={country.code}
+                      onClick={() => {
+                        randomColors();
+                        setSelectedItem((prevSelectedItem) =>
+                          prevSelectedItem &&
+                          prevSelectedItem.code === country.code
+                            ? null
+                            : country,
+                        );
+                      }}
+                      style={{
+                        backgroundColor:
+                          selectedItem?.code === country.code
+                            ? currentSelectedColor
+                            : undefined,
+                      }}
+                    >
+                      <img
+                        width={20}
+                        height={20}
+                        src={`https://raw.githubusercontent.com/onramper/small-open-datasets/master/rendered-country-flags/flags/${country.code}.png`}
+                        alt={country.code}
+                      />
+                      {country.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            ))
+          : countries?.map((country) => (
+              <li
+                key={country.code}
+                onClick={() => {
+                  randomColors();
+                  setSelectedItem((prevSelectedItem) =>
+                    prevSelectedItem && prevSelectedItem.code === country.code
+                      ? null
+                      : country,
+                  );
+                }}
+                style={{
+                  backgroundColor:
+                    selectedItem?.code === country.code
+                      ? currentSelectedColor
+                      : undefined,
+                }}
+              >
+                <img
+                  width={20}
+                  height={20}
+                  src={`https://raw.githubusercontent.com/onramper/small-open-datasets/master/rendered-country-flags/flags/${country.code}.png`}
+                  alt={country.code}
+                />
+                {country.name}
+              </li>
+            ))}
+      </div>
     </div>
   );
 }
--- src/constants/colors.tsx
+++ src/constants/colors.tsx
@@ -1,5 +1,24 @@
 const palette: any = {
   bgColor: "#d8e5e7",
-  selectColor: "#a2cbcb",
+  selectColor1: "#a2cbcb",
+  selectColor2: "#1e508a",
+  selectColor3: "#984869",
+  selectColor4: "#b77a5f",
+  selectColor5: "#7c8638",
+  selectColor6: "#7e6161",
+  selectColor7: "#9def94",
+  selectColor8: "#e3b5e8",
+  selectColor9: "#68a288",
 };
+export const selectedColors = [
+  palette.selectColor1,
+  palette.selectColor2,
+  palette.selectColor3,
+  palette.selectColor4,
+  palette.selectColor5,
+  palette.selectColor6,
+  palette.selectColor7,
+  palette.selectColor8,
+  palette.selectColor9,
+];
 export default palette;
